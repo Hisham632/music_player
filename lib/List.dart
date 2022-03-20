c3b4eabdab851906962ce4bc093d5e60c90307c3
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-03-20 01:15:26 -0400

    tempMetaData

--- lib/List.dart
+++ lib/List.dart
@@ -1,12 +1,17 @@
 // show the list of all songs in a driectory then make it so when they click on it they are directed to play page and it plays it
 
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:music_player/AudioPlayer_Playing.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
+import 'package:id3/id3.dart';
+import 'package:dart_tags/dart_tags.dart';
+import 'package:flutter_video_info/flutter_video_info.dart';
 
 class Lists extends StatefulWidget {
   const Lists({Key? key}) : super(key: key);
@@ -21,42 +26,74 @@ class _ListsState extends State<Lists> {
   late List<FileSystemEntity> listOfAllFolderAndFiles;
   final player  = AudioPlayer();
 
-  void initState()
-  {
-    int songNum=2;
+  //late Future<String> dataValue;
+
+initState()
+   {
+     int songNum=2;
 
     super.initState();
     Directory dir = Directory('/storage/emulated/0/AudioFiles/');
     listOfAllFolderAndFiles = dir.listSync(recursive: true);
-    //print(listOfAllFolderAndFiles);//has all the files from the directory
-    //String song = listOfAllFolderAndFiles[songNum].toString().substring(7, listOfAllFolderAndFiles[songNum].toString().length - 1);
+    print(listOfAllFolderAndFiles);//has all the files from the directory
+    print(listOfAllFolderAndFiles.length);//has all the files from the directory
+
+    String song = listOfAllFolderAndFiles[songNum].toString().substring(7, listOfAllFolderAndFiles[songNum].toString().length - 1);
     songName=listOfAllFolderAndFiles[songNum].toString().split('/').last.substring(0,listOfAllFolderAndFiles[songNum].toString().split('/').last.length-5);
     //print('line28');
     //print(song);
     print('line30');
-    print(songName);
-    var duration =  player.setFilePath(listOfAllFolderAndFiles[songNum].toString().substring(7, listOfAllFolderAndFiles[songNum].toString().length - 1));
-    print(duration);
-  }
+   // print(songName);
+   // var duration =  player.setFilePath(listOfAllFolderAndFiles[songNum].toString().substring(7, listOfAllFolderAndFiles[songNum].toString().length - 1));
+    //print(duration);
 
+   }
+
+
+
+  Future songMetaData(songNum) async{
+    //we could a list
+
+    String song = listOfAllFolderAndFiles[songNum].toString().substring(7, listOfAllFolderAndFiles[songNum].toString().length - 1);
+    print('line 71 '+song);
+
+    final metadata = await MetadataRetriever.fromFile(File('/storage/emulated/0/Download/Grand Escape (feat. Toko Miura).mp3'));
+
+    List metaInfo=[metadata.trackName, metadata.trackDuration, metadata.albumName, metadata.albumArtistName, metadata.year, metadata.albumArt];
+
+   /* String? trackName = metadata.trackName;
+    String? albumName = metadata.albumName;
+    String? albumArtistName = metadata.albumArtistName;
+    int? year = metadata.year;
+    Uint8List? albumArt = metadata.albumArt;
+    int? trackDuration = metadata.trackDuration;
+
+    //List<musicFileInfo> fileMeta=[];*/
+    print('Line84');
+    print(metadata.trackName);
+
+    return null;
+
+  }
 
 
   @override
   Widget build(BuildContext context) {
 
-
-
-
-
-
     return Scaffold(
       //appBar: AppBar(),
-      body: ListView.builder(
+      body: ListView.builder(//later add that divider
         itemCount: listOfAllFolderAndFiles.length,
           itemBuilder: (context,songNum){
             return Container(
               color: Colors.black,
-                  child: songListView(context, songNum),
+                  child: FutureBuilder(
+                    future: songMetaData(songNum),
+                    builder: (context, snapshot){
+                      return songListView(context, songNum);
+
+                    }
+                  )
 
             );
 
@@ -71,8 +108,9 @@ class _ListsState extends State<Lists> {
   songListView(context,songNum)
    {
 
+     //print('EachTime '+ songNum.toString());
 
-    return ListTile(//use the special textFont
+    return ListTile(//use the special textFont ALSO later add that divider
       leading: ClipRRect(
         borderRadius: BorderRadius.all(Radius.circular(10.0)),
         child: Image.asset('Images/img.png'),
@@ -102,4 +140,20 @@ class _ListsState extends State<Lists> {
 
 
 }
-//next thing make it so it goes to the playing tab and plays the clicked song
+
+
+
+
+
+/*
+class musicFileInfo
+{
+  final String trackName;
+  final String albumName;
+  final String albumArtistName;
+  final int year;
+  final Uint8List albumArt;
+
+  musicFileInfo(this.trackName, this.albumName, this.albumArtistName,this.year, this.albumArt);
+}*/
+//next thing sync up each vid time and artist name and the pic, then use the divider(maybe add whiteLine)
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:music_player/List.dart';
 
 void main() {
   runApp( MaterialApp(
-    initialRoute:'/test' ,
+    initialRoute:'/listing' ,
     routes: {
       '/':(context)=> AudioPlay(number: 2),
       '/listing':(context)=> Lists(),
@@ -24,4 +24,13 @@ Music Player App:
 3.a page where we select the song played, list of songs displayed in a list
 
 4.Playlist page where we can add songs from the list to a playlist
- */
+
+What to do next:
+Leave the code for the metadata just return null for now
+add the list divider thing
+make it nice
+create the album/playlist page where each folder becomes a grid and when u click it opens a new page as a list
+
+do th avg background color where based on those 4 it switches
+add time stamps below slider
+*/
